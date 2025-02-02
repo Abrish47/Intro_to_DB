b599468ff4d4453d6b99d008acc7ff3d492ae3d9
Abrish47 <abrahamkifle0117@gmail.com>
2025-02-02 22:03:39 +0300

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,47 @@
+-- Create the database
+CREATE DATABASE alx_book_store;
+
+-- Use the newly created database
+USE alx_book_store;
+
+-- Create the Authors table
+CREATE TABLE AUTHORS (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create the Books table
+CREATE TABLE BOOKS (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id) ON DELETE SET NULL
+);
+
+-- Create the Customers table
+CREATE TABLE CUSTOMERS (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- Create the Orders table
+CREATE TABLE ORDERS (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id) ON DELETE CASCADE
+);
+
+-- Create the Order_Details table
+CREATE TABLE ORDER_DETAILS (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL CHECK (quantity > 0),
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id) ON DELETE CASCADE
+);
